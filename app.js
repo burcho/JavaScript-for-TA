91013afbab5a45fda00a6f7eafed41272d4c4e6a
burcho <91255631+burcho@users.noreply.github.com>
2023-11-06 22:59:42 +0100

    Homework_1

---
+++ app.js
@@ -0,0 +1,47 @@
+// task 2 
+// console.log("Burchak");
+
+// // task 4 
+
+// let position = 'Head of Office';
+// let salary;
+
+// const Worker = {
+//     name: "Bobby",
+//     age: 25,
+//     MaritalStatus: true,
+//     position: null,
+//     salary: (salary)
+// }
+
+// // task 5 
+// let isAdult = confirm("Please confirm that you already reached the age of majority");
+// console.log(isAdult);
+
+// //task 6
+//  let firstName;
+//  let lastName;
+//  let studyGroup;
+//  let yearOfBirth;
+
+//  firstName = "Olha";
+//  lastName = "Burchak";
+//  studyGroup = "JavaScript for TA";
+//  yearOfBirth = 1998;
+
+//  let maritalStatusNew = Boolean;
+//  maritalStatusNew = false;
+
+//  console.log(yearOfBirth, maritalStatusNew, firstName, lastName, studyGroup);// as task was display only variables I didn't add their names to console
+
+//  let person;
+//  person = null;
+//  let friend;
+//  friend = (friend);
+
+//  console.log(person, friend);// as task was display only variables I didn't add their names to console
+
+
+
+
+
